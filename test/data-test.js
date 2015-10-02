de614ead15ff6081b05449551cd1926a6636784b
Dumitru Cantea <canteadumitru@gmail.com>
2015-10-02 23:21:32 +0300

    new data file format: txt, + rename_concepts

--- test/data-test.js
+++ test/data-test.js
@@ -6,18 +6,26 @@ var assert = require('assert');
 var LANGUAGES = data.getLanguages();
 var NAMES = data.getNames();
 
+function getData(name, lang, country) {
+	try {
+		return data.get(name, lang, country);
+	} catch (e) {
+		console.log('error on: ', lang, name, e.message);
+		throw e;
+	}
+}
+
 describe('data', function() {
 	it('validation', function() {
 		LANGUAGES.forEach(function(lang) {
 			NAMES.forEach(function(name) {
-				var result;
-				try {
-					result = data.get(lang, name);
-				} catch (e) {
-					console.log('error on: ', lang, name, e.message);
-					throw e;
-				}
+				var result = getData(name, lang);
 				assert.ok(result);
+				var countries = data.getCountries(lang);
+				countries.forEach(function(country) {
+					result = getData(name, lang, country);
+					assert.ok(result);
+				});
 			});
 		});
 	});
