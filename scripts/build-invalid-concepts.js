2d1d29004480b3b70eba1aad9878a7d9b71390ef
Dumitru Cantea <canteadumitru@gmail.com>
2018-03-26 09:01:56 +0300

    added stopwords to invalid_concepts

---
+++ scripts/build-invalid-concepts.js
@@ -0,0 +1,18 @@
+
+const stopwords = require('stopwords-json');
+const atonic = require('atonic');
+const getLanguages = require('../lib').getLanguages;
+const fs = require('fs');
+const join = require('path').join;
+
+for (let lang of getLanguages()) {
+    const file = join(__dirname, '../data', lang, 'invalid_concepts.txt');
+    let invalidConcepts = fs.readFileSync(file, 'utf8').split(/\n+/g);
+    invalidConcepts = invalidConcepts.concat(stopwords[lang]);
+    invalidConcepts = invalidConcepts.filter(item => !!item && item.trim().length);
+    invalidConcepts = invalidConcepts.map(item => atonic.lowerCase(item.toLowerCase().trim()));
+    // uniq
+    invalidConcepts = invalidConcepts.filter((value, index, self) => self.indexOf(value) === index);
+    invalidConcepts = invalidConcepts.sort();
+    fs.writeFileSync(file, invalidConcepts.join('\n'), 'utf8');
+}
